a683a9c5763c491b536af2fc0bdc07003d9a52e3
Zack <zack@regattawagon.com>
2024-04-03 22:27:45 -0400

    pushing changes before bed

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pic_type
+
+go 1.21.5
--- main.go
+++ main.go
@@ -1,49 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "net/http"
-    "path/filepath"
-		//"reflect"
+	"log"
+	"net/http"
+	"os"
+	//"reflect"
 )
 
-func main() {
-    var files []string
-
-    root := "/home/zack-dt/pics"
-
-    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-        files = append(files, path)
-        return nil
-    })
-    if err != nil {
-        panic(err)
-    }
-
-    for _, file := range files {
-        //fmt.Println(reflect.TypeOf(file).String()+":\t"+file)
-
-        f, err := os.Open(file)
-				//fmt.Println(reflect.TypeOf(f).String()+":\t")
-      	if err != nil {
-      		panic(err)
-      	}
-        // Get the content
-
-      	contentType, err := GetFileContentType(f)
-				// why is this being weird?
-				if err != nil {
-          fmt.Println(err)
-      		//panic(err)
-      	}
-
-				if  (contentType != "image/jpeg" && contentType != "image/png" && contentType != "image/gif") {
-      		//fmt.Println("File:\t"+file+"\tContent Type:\t" + contentType)
-				}
-
-      	f.Close()
-    }
+type ApplicationConfiguration struct {
+	ConfigFile string
+	PictureDir string
 }
 
 func GetFileContentType(out *os.File) (string, error) {
@@ -55,10 +21,44 @@ func GetFileContentType(out *os.File) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
 	// Use the net/http package's handy DectectContentType function. Always returns a valid
 	// content-type by returning "application/octet-stream" if no others seemed to match.
 	contentType := http.DetectContentType(buffer)
-  fmt.Println(contentType)
+
+	log.Println(contentType)
+
 	return contentType, nil
+}
+
+func (a *ApplicationConfiguration) buildFileList() []string {
+	var fileList []string
+	f, err := os.Open(a.PictureDir)
+	if err != nil {
+		log.Println(err)
+
+	}
+	files, err := f.Readdir(0)
+	if err != nil {
+		log.Println(err)
+
+	}
+
+	for _, v := range files {
+
+		if !v.IsDir() {
+			fileList = append(fileList, v.Name())
+			log.Printf("File Path: %v/%v", a.PictureDir, v.Name())
+		}
+	}
+	return fileList
+}
+
+func main() {
+
+	a := &ApplicationConfiguration{
+		ConfigFile: "config.yaml",
+		PictureDir: "/home/zack/Pictures",
+	}
+
+	log.Println(a.buildFileList())
 }
